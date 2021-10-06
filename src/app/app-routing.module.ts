fc06fd80b94cebf28ea4c22f94c36946494370cc
Heikki Lett <lettheikki@gmail.com>
2021-10-06 16:11:36 +0300

    routerLink, app-routing, css, html algus

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // URL path brauseris: "localhost:4200", HomeComponent sisu koos HTML-ga,
+                          // mida n2idatakse kasutajale CSS-ga kujundatakse
+                          // .ts failidega lisatakse dynaamilisust
+  { path: "", component: HomeComponent },
+  // kui on brauseris "localhost:4200/ostukorv", siis n2itab cart.compnonent.html-i ja lisasid
+  { path: "ostukorv", component: CartComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
